3badfca3676260e4929fb92297227fa701e60682
Victor Vilela <vilela_victor@hotmail.com>
2019-06-30 21:31:08 -0500

    first angular component

--- getting-started/src/app/app.component.ts
+++ getting-started/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector : 'pm-root',
+  template : `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <div>My firsrt component</div>
+    </div>
+  `,
 })
-export class AppComponent {
-  title = 'Angular: Getting Started';
+export class  AppComponent {
+  pageTitle : string = 'Acme Product Management'
 }
