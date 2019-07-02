27eafb4262ccedb7585113d777152edc5365928d
Victor Vilela <vilela_victor@hotmail.com>
2019-07-01 22:27:16 -0500

    two-way binding

--- getting-started/src/app/app.module.ts
+++ getting-started/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-lit.component';
@@ -10,7 +11,8 @@ import { ProductListComponent } from './products/product-lit.component';
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
