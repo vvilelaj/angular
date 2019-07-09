6e7054c5f674c9b37c085a5049cf6e08edb7253b
vvilelaj <vilela_victor@hotmail.com>
2019-07-09 11:19:19 -0500

    extract a routing module

--- getting-started/src/app/app.module.ts
+++ getting-started/src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: "welcome", component: WelcomeComponent },
-      { path: "", redirectTo: "welcome", pathMatch: 'full' },
-      { path: "**", redirectTo: "welcome", pathMatch: 'full' }
-    ]),
-    ProductModule
+    ProductModule,
+    RoutingModule
   ],
   declarations: [
     AppComponent,
