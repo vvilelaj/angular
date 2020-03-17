d3ab4265eb915a000388790e2b901ca890e8444f
vvilelaj <victor.vilela@outlook.com>
2020-03-17 09:57:34 -0500

    consultant-portal-app : add styles to header and foorte component inside consultat feature module

--- 4.demos/consultant-portal-app/src/app/consultant/consultant.module.ts
+++ 4.demos/consultant-portal-app/src/app/consultant/consultant.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from './../shared/shared.module';
 import { ConsultantRoutingModule } from './consultant-routing.module';
-import { ProfileInfoComponent } from './profile/profile-info/profile-info.component';
 import { ConsultantComponent } from './consultant.component';
-import { ProfileEditComponent } from './profile/profile-edit/profile-edit.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { ProfileInfoComponent } from './profile/profile-info/profile-info.component';
+import { ProfileEditComponent } from './profile/profile-edit/profile-edit.component';
+
+
+
 
 
 @NgModule({
